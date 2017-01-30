11ff1eea3add6bb2144e418fae8393a0e274e812
Joram Berger <joram.berger@gmx.de>
2017-01-30 11:44:03 +0100

    Improve translation script

--- translate.py
+++ translate.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python3
 """
 
-Usage: translate.py de
+Usage: translate.py language
+Example: translate.py de
 """
+
 import sys
 import re
+import os
 import yaml
 
-pattern1 = 'dlg:value="%s"'
-pattern2 = 'dlg:value="%s"'
 
-with open(sys.argv[1] + '.yaml') as f:
+pattern1 = 'dlg:value="%s"'
+pattern2 = 'dlg:value="%s:"'
+filename = "OOoLilyPond/GUI_Editor.xdl"
+fileorig = filename + '.orig'
+language = sys.argv[1]
+if language == 'en':
+    if os.path.exists(fileorig):
+        os.rename(fileorig, filename)
+    else:
+        print("File %s does not exist. Please use git checkout." % fileorig)
+    exit(0)
+
+with open(language + '.yaml') as f:
     dictionary = yaml.load(f)
 
-with open("OOoLilyPond/GUI_Editor.xdl") as f:
+with open(filename) as f:
     text = f.read()
+with open(fileorig, 'w') as f:
+    f.write(text)
 
 for original, translation in dictionary.items():
     text = text.replace(pattern1 % original, pattern1 % translation)
     text = text.replace(pattern2 % original, pattern2 % translation)
 
-with open("OOoLilyPond/GUI_Editor.xdl.de", 'w') as f:
+with open(filename, 'w') as f:
     f.write(text)
-
-#dlg:value="Ly Output"
